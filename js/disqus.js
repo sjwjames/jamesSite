2e7250aed680c08b0375e83f17182635ccf6d5c8
Jianwei <sjwjames@126.com>
2016-02-21 16:19:50 +0800

    add disqus and icon

---
+++ js/disqus.js
@@ -0,0 +1,11 @@
+// var disqus_config=function(){
+//   this.page.url="sjwjames.com";
+//   this.page.identifier=jamessite0325;
+// }
+
+(function(){
+  var d=document,s=d.createElement("script");
+  s.src="//jamessite0325.disqus.com/embed.js";
+  s.setAttribute('date-timestamp',+new Date());
+  (d.head||d.body).appendChild(s);
+})();
